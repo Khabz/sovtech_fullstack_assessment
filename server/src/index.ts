2ecd1a6f20c779ffdfe630e14febd087c142d7ff
khabubundivhu <khabubundivh@gmail.com>
2021-10-15 11:01:24 +0200

    Test prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,42 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express, { Request, Response } from 'express';
+import { createServer } from 'http';
 import dotenv from 'dotenv-safe';
+import cors from 'cors';
+import compression from 'compression';
+import path from 'path';
 import schema from '@src/graphql/schemas/schema';
 import { performAstCodegen } from '@src/codegen';
 
 dotenv.config();
+const PORT = process.env.PORT || 4000;
 
-const startServer = () => {
+const startServer = async () => {
+  const app = express();
   const server = new ApolloServer({
     schema,
   });
 
+  app.use(cors());
+  app.use(compression());
+
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+  const httpServer = createServer(app);
+  // Perform codegen when server starts
   performAstCodegen();
 
-  server
-    .listen()
-    .then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}graphql`);
-    })
-    .catch(err => console.log('Error launching server', err));
+  app.use(express.static('public'));
+
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  });
+
+  httpServer.listen(PORT, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 };
 
 startServer();
